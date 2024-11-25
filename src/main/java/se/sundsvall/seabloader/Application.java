be137d66c569180d73ab23c8a87ce7f09104b12d
Andreas Carlsson <andreas.carlsson@sundsvall.se>
2024-11-25 09:05:36 +0100

    Fixed Shedlock bean load issue

--- src/main/java/se/sundsvall/seabloader/Application.java
+++ src/main/java/se/sundsvall/seabloader/Application.java
@@ -5,16 +5,12 @@ import static org.springframework.boot.SpringApplication.run;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 import se.sundsvall.dept44.ServiceApplication;
-
-import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 
 @ServiceApplication
 @EnableFeignClients
 @EnableScheduling
 @EnableAsync
-@EnableSchedulerLock(defaultLockAtMostFor = "PT2M")
 public class Application {
 	public static void main(final String... args) {
 		run(Application.class, args);
--- src/main/java/se/sundsvall/seabloader/config/ShedLockConfig.java
+++ src/main/java/se/sundsvall/seabloader/config/ShedLockConfig.java
@@ -1,16 +1,16 @@
 package se.sundsvall.seabloader.config;
 
 import javax.sql.DataSource;
-
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import net.javacrumbs.shedlock.core.LockProvider;
-import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import se.sundsvall.dept44.util.jacoco.ExcludeFromJacocoGeneratedCoverageReport;
 
 @Configuration
+@EnableSchedulerLock(defaultLockAtMostFor = "PT2M")
 @ExcludeFromJacocoGeneratedCoverageReport
 class ShedLockConfig {
 
