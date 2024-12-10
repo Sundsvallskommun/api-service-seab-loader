2f4ab4b8d0131db06ed54f65b78b1da37ae5461d
cheezi0747 <linus.sjolinder@b3.se>
2024-12-10 13:24:07 +0100

    UF-11274: Update dept44

--- src/test/java/se/sundsvall/seabloader/api/InvoicesResourceTest.java
+++ src/test/java/se/sundsvall/seabloader/api/InvoicesResourceTest.java
@@ -11,8 +11,8 @@ import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import se.sundsvall.seabloader.service.InvoiceService;
 
