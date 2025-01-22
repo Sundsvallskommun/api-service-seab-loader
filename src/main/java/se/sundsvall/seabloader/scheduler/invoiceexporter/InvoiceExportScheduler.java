24f60732ec490dd6a523e6b3a51b5782a4029f11
cheezi0747 <linus.sjolinder@b3.se>
2025-01-22 15:40:49 +0100

    UF-11786: Use dept44-scheduler

--- src/main/java/se/sundsvall/seabloader/scheduler/invoiceexporter/InvoiceExportScheduler.java
+++ src/main/java/se/sundsvall/seabloader/scheduler/invoiceexporter/InvoiceExportScheduler.java
@@ -4,13 +4,11 @@ import static org.springframework.util.CollectionUtils.isEmpty;
 import static se.sundsvall.seabloader.integration.db.model.enums.Status.EXPORT_FAILED;
 import static se.sundsvall.seabloader.integration.db.model.enums.Status.UNPROCESSED;
 
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import se.sundsvall.dept44.requestid.RequestId;
+import se.sundsvall.dept44.scheduling.Dept44Scheduled;
 import se.sundsvall.seabloader.integration.db.InvoiceRepository;
 import se.sundsvall.seabloader.integration.db.model.InvoiceId;
 import se.sundsvall.seabloader.integration.db.model.enums.Status;
@@ -27,9 +25,6 @@ public class InvoiceExportScheduler extends AbstractScheduler {
 		UNPROCESSED, EXPORT_FAILED
 	};
 
-	private static final String LOG_SENDING_STARTED = "Beginning of sending invoices to Invoice-cache";
-	private static final String LOG_SENDING_ENDED = "Sending of invoices to Invoice-cache has ended";
-
 	private final InvoiceService invoiceService;
 	private final InvoiceRepository invoiceRepository;
 
@@ -39,13 +34,12 @@ public class InvoiceExportScheduler extends AbstractScheduler {
 	}
 
 	@Override
-	@Scheduled(cron = "${scheduler.invoiceexporter.cron.expression}")
-	@SchedulerLock(name = "exportinvoices", lockAtMostFor = "${scheduler.shedlock-lock-at-most-for}")
+	@Dept44Scheduled(
+		cron = "${scheduler.invoiceexporter.cron.expression}",
+		name = "${scheduler.invoiceexporter.name}",
+		lockAtMostFor = "${scheduler.shedlock-lock-at-most-for}",
+		maximumExecutionTime = "${scheduler.maximum-execution-time}")
 	public void execute() {
-		RequestId.init();
-
-		LOGGER.info(LOG_SENDING_STARTED);
-		LOGGER.info("Exporting invoices to InvoiceCache");
 
 		final var invoiceIdsToSend = invoiceRepository.findIdsByStatusIn(STATUSES_OF_INVOICES_TO_SEND);
 
@@ -61,6 +55,5 @@ public class InvoiceExportScheduler extends AbstractScheduler {
 			.map(InvoiceId::getId)
 			.forEach(invoiceService::sendInvoiceToInvoiceCache);
 
-		LOGGER.info(LOG_SENDING_ENDED);
 	}
 }
